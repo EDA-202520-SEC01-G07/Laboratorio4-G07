6b974d724df390b5b29dff2c504f93f7d0d8f8c2
jlozadab <j.lozadab@uniandes.edu.co>
2025-08-29 07:51:43 -0500

    pop

--- DataStructures/Stack/Stack.py
+++ DataStructures/Stack/Stack.py
@@ -2,7 +2,13 @@ from DataStructures.List import single_linked_list as sl
 def new_stack():
     pila = sl.new_list()
     return pila
-
+def pop(stack):
+    if sl.is_empty(stack):
+        raise Exception("EmptyStructureError: queue is empty")
+    else:
+        elemento = sl.last_element(stack)
+        sl.remove_last(stack)
+        return elemento
 def push(pila, elemento):
     sl.add_last(pila, elemento)
     return pila
