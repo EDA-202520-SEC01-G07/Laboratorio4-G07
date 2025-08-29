ecbde0e0e7dfac7a64111b9dfc5d7023caa2e059
Juliana-_Rod-1234 <js.rodriguezm1234@uniandes.edu.co>
2025-08-29 07:41:45 -0500

    funciones empty y size

--- DataStructures/Queue/Queue.py
+++ DataStructures/Queue/Queue.py
@@ -1,11 +1,15 @@
 from DataStructures.List import array_list as lt
 
 def new_queue():
-    cola = sl.new_list()
+    cola = lt.new_list()
     return cola
 def enqueue(cola, elemento):
     lt.add_last(cola, elemento)
     return cola
 
-def is_empty():
-    
+def is_empty(cola):
+    x = lt.is_empty(cola)
+    return x
+
+def size(cola):
+    return lt.size(cola)
