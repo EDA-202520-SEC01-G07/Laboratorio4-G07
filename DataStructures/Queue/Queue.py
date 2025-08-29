73b499b0934dc223e6fce61a8f57d50278a02d2d
jlozadab <j.lozadab@uniandes.edu.co>
2025-08-29 07:39:39 -0500

    enqueue

--- DataStructures/Queue/Queue.py
+++ DataStructures/Queue/Queue.py
@@ -3,6 +3,9 @@ from DataStructures.List import array_list as lt
 def new_queue():
     cola = sl.new_list()
     return cola
+def enqueue(cola, elemento):
+    lt.add_last(cola, elemento)
+    return cola
 
 def is_empty():
     
